041f34846e1e1c7b80ef5b00b43254eb9c939dcd
George Hilliard <thirtythreeforty@gmail.com>
2015-12-18 21:56:42 -0600

    Add background tick marks

--- src/consensus.c
+++ src/consensus.c
@@ -8,6 +8,7 @@ static Window *window;
 static FaceLayer *face_layer;
 static Layer *black_layer;
 static DateComplication *date_complication;
+static GDrawCommandImage *ticks_image;
 static BatteryComplication *battery_complication;
 
 void on_tick(struct tm *tick_time, TimeUnits units_changed)
@@ -40,6 +41,8 @@ static void fill_black(Layer *layer, GContext *ctx)
 	GRect rect = layer_get_bounds(layer);
 	graphics_context_set_fill_color(ctx, GColorBlack);
 	graphics_fill_rect(ctx, rect, 0, 0);
+
+	gdraw_command_image_draw(ctx, ticks_image, GPointZero);
 }
 
 static void init_layers(void)
@@ -47,23 +50,31 @@ static void init_layers(void)
 	GRect size = layer_get_bounds(window_get_root_layer(window));
 	GPoint center = grect_center_point(&size);
 
+	ticks_image = gdraw_command_image_create_with_resource(RESOURCE_ID_TICKS);
+
 	black_layer = layer_create(size);
 	layer_set_update_proc(black_layer, fill_black);
 	layer_add_child(window_get_root_layer(window), black_layer);
 
 	const unsigned int complication_size = 51;
+	const unsigned int complication_offset_x = PBL_IF_ROUND_ELSE(15, 10);
+	const unsigned int complication_offset_y = 15;
 
 	BatteryChargeState charge_state = battery_state_service_peek();
 	const GRect battery_complication_position =
-		{{ .x = center.x - complication_size - 15, .y = center.y - complication_size / 2 },
-		 { .h = complication_size, .w = complication_size }};
+		{{ .x = center.x - complication_size - complication_offset_x,
+		   .y = center.y - complication_size / 2 },
+		 { .h = complication_size,
+		   .w = complication_size }};
 	battery_complication = battery_complication_create(battery_complication_position, &charge_state);
 	layer_add_child(window_get_root_layer(window), battery_complication_get_layer(battery_complication));
 	battery_complication_animate_in(battery_complication);
 
 	const GRect date_complication_position =
-		{{ .x = center.x - complication_size / 2, .y = center.y + 15 },
-		 { .h = complication_size, .w = complication_size }};
+		{{ .x = center.x - complication_size / 2,
+		   .y = center.y + complication_offset_y },
+		 { .h = complication_size,
+		   .w = complication_size }};
 	date_complication = date_complication_create(date_complication_position);
 	layer_add_child(window_get_root_layer(window), date_complication_get_layer(date_complication));
 	date_complication_animate_in(date_complication);
@@ -80,6 +91,7 @@ static void deinit_layers(void)
 	date_complication_destroy(date_complication);
 	battery_complication_destroy(battery_complication);
 	layer_destroy(black_layer);
+	free(ticks_image);
 }
 
 static void main_window_load(Window *window)
