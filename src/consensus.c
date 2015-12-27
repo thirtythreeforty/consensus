d3a9a22224e7a62cd30f1f4a44c0f59a2ee95b24
George Hilliard <thirtythreeforty@gmail.com>
2015-12-27 00:37:47 -0600

    Display white "no connection" symbol on disconnect

--- src/consensus.c
+++ src/consensus.c
@@ -8,8 +8,11 @@ static Window *window;
 static FaceLayer *face_layer;
 static Layer *black_layer;
 static DateComplication *date_complication;
-static GDrawCommandImage *ticks_image;
+static BitmapLayer *no_bluetooth_layer;
 static BatteryComplication *battery_complication;
+
+static GDrawCommandImage *ticks_image;
+static GBitmap *no_bluetooth_image;
 
 void on_tick(struct tm *tick_time, TimeUnits units_changed)
 {
@@ -48,6 +51,15 @@ void on_battery_state_change(BatteryChargeState charge)
 	}
 }
 
+void on_connection_change(bool connected)
+{
+	layer_set_hidden(bitmap_layer_get_layer(no_bluetooth_layer), connected);
+}
+
+void ignore_connection_change(bool connected)
+{
+}
+
 static void fill_black(Layer *layer, GContext *ctx)
 {
 	GRect rect = layer_get_bounds(layer);
@@ -63,6 +75,7 @@ static void init_layers(void)
 	GPoint center = grect_center_point(&size);
 
 	ticks_image = gdraw_command_image_create_with_resource(RESOURCE_ID_TICKS);
+	no_bluetooth_image = gbitmap_create_with_resource(RESOURCE_ID_NO_BLUETOOTH);
 
 	black_layer = layer_create(size);
 	layer_set_update_proc(black_layer, fill_black);
@@ -71,6 +84,19 @@ static void init_layers(void)
 	const unsigned int complication_size = 51;
 	const unsigned int complication_offset_x = PBL_IF_ROUND_ELSE(15, 10);
 	const unsigned int complication_offset_y = 15;
+
+	const GRect bluetooth_image_size = gbitmap_get_bounds(no_bluetooth_image);
+	const GRect bluetooth_layer_location =
+		{{ .x = center.x - bluetooth_image_size.size.w / 2,
+		   .y = center.y - complication_offset_y - complication_size / 2 - bluetooth_image_size.size.h / 2 },
+		 { .h = bluetooth_image_size.size.h,
+		   .w = bluetooth_image_size.size.w }};
+	no_bluetooth_layer = bitmap_layer_create(bluetooth_layer_location);
+	bitmap_layer_set_bitmap(no_bluetooth_layer, no_bluetooth_image);
+	bitmap_layer_set_compositing_mode(no_bluetooth_layer, GCompOpSet);
+	// Immediately hide or show the icon
+	on_connection_change(connection_service_peek_pebble_app_connection());
+	layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(no_bluetooth_layer));
 
 	BatteryChargeState charge_state = battery_state_service_peek();
 	const GRect battery_complication_position =
@@ -103,6 +129,8 @@ static void deinit_layers(void)
 	face_layer_destroy(face_layer);
 	date_complication_destroy(date_complication);
 	battery_complication_destroy(battery_complication);
+	bitmap_layer_destroy(no_bluetooth_layer);
+	gbitmap_destroy(no_bluetooth_image);
 	layer_destroy(black_layer);
 	free(ticks_image);
 }
@@ -132,6 +160,12 @@ static void init(void)
 
 	accel_tap_service_subscribe(on_tap);
 
+	static const ConnectionHandlers conn_handlers = {
+		.pebble_app_connection_handler = on_connection_change,
+		.pebblekit_connection_handler = ignore_connection_change,
+	};
+	connection_service_subscribe(conn_handlers);
+
 	time_t abs_time = time(0);
 	struct tm *tick_time = localtime(&abs_time);
 	on_tick(tick_time, MINUTE_UNIT);
@@ -140,6 +174,7 @@ static void init(void)
 static void deinit(void)
 {
 	animation_unschedule_all();
+	connection_service_unsubscribe();
 	accel_tap_service_unsubscribe();
 	battery_state_service_unsubscribe();
 	tick_timer_service_unsubscribe();
