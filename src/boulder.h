a79fec00b14418c654be87fbdcbe67df300911ec
George Hilliard <thirtythreeforty@gmail.com>
2016-01-18 01:19:43 -0600

    Allow PropertyAnimation to use Pebble SDK update functions

--- src/boulder.h
+++ src/boulder.h
@@ -113,11 +113,78 @@ public:
 	}
 };
 
-template<typename S, typename T>
-using PropertyAnimationSetter = void (*)(S&, const T&);
 
-template<typename S, typename T>
-using PropertyAnimationGetter = const T& (*)(const S&);
+/*  The PropertyAnimation class provides a type-safe abstraction over the
+ *  Pebble SDK's type-erased version.  It handles all the update function
+ *  boilerplate needed for any type being animated.  Users only need to define
+ *  a suitable interpolation function for the type T:
+ *      T interpolate(uint32_t distance, T& from, T& to);
+ *  This implementation also utilizes Pebble's built-in update methods for all
+ *  types they provide implementations for, so using this wrapper with such a
+ *  type incurs no code size overhead and does not require the definition of an
+ *  interpolate() function.
+ */
+namespace {
+	// The code in this namespace{} is helper code for the PropertyAnimation class.
+
+	template<typename S, typename T>
+	using PropertyAnimationSetter = void (*)(S&, const T&);
+
+	template<typename S, typename T>
+	using PropertyAnimationGetter = const T& (*)(const S&);
+
+	using updater_t = void (*)(::PropertyAnimation*, const uint32_t);
+
+	template<typename S, typename T, PropertyAnimationSetter<S, T> Setter>
+	struct PAUpdate {
+		/* This struct is particularly interesting.  It provides the default
+		 * templated implementation of the update function (templates being
+		 * necessary to ensure type-safety, exactly as if the user were
+		 * specifying it).  Such code is needed for every type T being animated
+		 * and is quite boring -- the only interesting parts will happen with
+		 * the actual interpolation and storing the calculated value.  The user
+		 * supplies both interesting functions.  This struct also provides
+		 * specializations for Pebble's built-in types, which allows us to
+		 * select their implementation for free at compile time, exactly as if
+		 * the user were manually specifying the
+		 * PropertyAnimationImplementation.
+		 */
+
+		static void update(::PropertyAnimation *panim, const uint32_t distance) {
+			T from, to;
+			property_animation_from(panim, &from, sizeof(T), false);
+			property_animation_to(panim, &to, sizeof(T), false);
+
+			T interpolated = interpolate(distance, from, to);
+
+			auto anim = reinterpret_cast<::Animation*>(panim);
+			auto impl = reinterpret_cast<const ::PropertyAnimationImplementation*>(animation_get_implementation(anim));
+			auto setter = reinterpret_cast<PropertyAnimationSetter<S, T>>(impl->accessors.setter.int16);
+
+			S *subject;
+			property_animation_subject(panim, reinterpret_cast<void**>(&subject), false);
+
+			// subject cannot be null, because it was a reference in the PropertyAnimation ctor
+			// setter cannot be null, because it was statically checked in the ctor
+			setter(*subject, interpolated);
+		}
+	};
+
+#define B_SPECIALIZE_PAUPDATE(Type, BuiltInFunc) \
+	template<typename S, PropertyAnimationSetter<S, Type> Setter> \
+	struct PAUpdate<S, Type, Setter> { \
+		static constexpr updater_t update = BuiltInFunc; \
+	};
+
+	B_SPECIALIZE_PAUPDATE(int16_t, property_animation_update_int16);
+	B_SPECIALIZE_PAUPDATE(uint32_t, property_animation_update_uint32);
+	B_SPECIALIZE_PAUPDATE(::GPoint, property_animation_update_gpoint);
+	B_SPECIALIZE_PAUPDATE(::GRect, property_animation_update_grect);
+	B_SPECIALIZE_PAUPDATE(::GColor8, property_animation_update_gcolor8);
+
+#undef B_SPECIALIZE_PAUPDATE
+
+}
 
 template<typename S, typename T, PropertyAnimationSetter<S, T> Setter, PropertyAnimationGetter<S, T> Getter>
 class PropertyAnimation {
@@ -125,10 +192,13 @@ class PropertyAnimation {
 
 public:
 	PropertyAnimation(S& subject, const T* from, const T* to) {
+		static_assert(Setter != nullptr, "Setter cannot be null");
+		static_assert(Getter != nullptr, "Getter cannot be null");
+
 		static const PropertyAnimationImplementation impl = {
 			.base = {
 				NULL,
-				(AnimationUpdateImplementation)PropertyAnimation::update,
+				(AnimationUpdateImplementation)PAUpdate<S, T, Setter>::update,
 				NULL,
 			},
 			.accessors = {
@@ -162,25 +232,6 @@ public:
 
 	operator ::PropertyAnimation*() {
 		return anim;
-	}
-
-private:
-	static void update(::PropertyAnimation *panim, const uint32_t distance) {
-		T from, to;
-		property_animation_from(panim, &from, sizeof(T), false);
-		property_animation_to(panim, &to, sizeof(T), false);
-
-		T interpolated = interpolate(distance, from, to);
-
-		auto impl = reinterpret_cast<const PropertyAnimationImplementation*>(animation_get_implementation(reinterpret_cast<::Animation*>(panim)));
-		auto setter = reinterpret_cast<PropertyAnimationSetter<S, T>>(impl->accessors.setter.int16);
-
-		S *subject;
-		property_animation_subject(panim, reinterpret_cast<void**>(&subject), false);
-
-		if(setter && subject) {
-			setter(*subject, interpolated);
-		}
 	}
 };
 
