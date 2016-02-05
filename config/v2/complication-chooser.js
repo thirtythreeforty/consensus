a9da1d7e866bd28f6fbd94a695303e5476b6e05c
George Hilliard <thirtythreeforty@gmail.com>
2016-02-05 15:50:01 -0600

    Sync from riotjs

---
+++ config/v2/complication-chooser.js
@@ -0,0 +1,56 @@
+<complication-customize>
+</complication-customize>
+
+<complication-customize-health>
+</complication-customize-health>
+
+<complication-customize-weather>
+</complication-customize-weather>
+
+<complication-chooser>
+	<div class='item-container-content'>
+		<label class="item">
+			<span>{ opts.position }</span>
+			<select dir='rtl' class="item-select" name='typechooser' onchange={ ontypechosen } value={ chosentype }>
+				<option class="item-select-option">None</option>
+				<option class="item-select-option">Battery</option>
+				<option class="item-select-option">Health</option>
+				<option class="item-select-option">Date</option>
+				<option class="item-select-option">Weather</option>
+			</select>
+		</label>
+		<complication-customize name='customizer' />
+	</div>
+
+	<script>
+	changecustomizer() {
+		// Get the correct name of the customizer, or use the empty one
+		var whichComplication = {
+			"Battery": undefined,
+			"Health": "complication-customize-health",
+			"Date": undefined,
+			"Weather": "complication-customize-weather"
+		}[this.chosentype] || "complication-customize";
+
+		// Mount it
+		riot.mount(this.tags['customizer'].root, whichComplication, { foo: opts.cname});
+	}
+
+	ontypechosen(e) {
+		this.chosentype = e.target.value;
+		this.changecustomizer();
+	}
+
+	this.chosentype = "Health";
+	this.on('mount', this.changecustomizer);
+	</script>
+</complication-chooser>
+
+<complication-chooser-list>
+	<div class='item-container-header'>Complications</div>
+	<complication-chooser each={ names } position={ p }/>
+
+	function makeName(s) { return { p: s }; }
+	this.names = [ 'Left', 'Bottom', 'Right' ].map(makeName);
+</complication-chooser-list>
+
