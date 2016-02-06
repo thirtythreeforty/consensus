2802d8bcbc79f47e5d514f11512f571ac5d70653
George Hilliard <thirtythreeforty@gmail.com>
2016-02-06 12:04:51 -0600

    Ditch ES6 features

--- config/v2/containers.js
+++ config/v2/containers.js
@@ -10,11 +10,11 @@ riot.tag2('configuration-footer', '<div class="item-container-footer"> <yield></
 riot.tag2('configuration-toggle', '<label class="item"> <yield></yield> <input id="{inputid}" type="checkbox" class="item-toggle"></input> </label>', '', '', function(opts) {
 
 
-	this.one('mount', () => $(this.root).find('.item-toggle').itemToggle());
+	this.one('mount', function() { $(this.root).find('.item-toggle').itemToggle(); });
 }, '{ }');
 
 riot.tag2('configuration-dropdown', '<label class="item"> {opts.caption} <select dir="rtl" class="item-select" name="{opts.selectname}" onchange="{opts.onchange}" value="{opts.value}"> <yield></yield> </select> </label>', '', '', function(opts) {
-	this.one('mount', () => $(this.root).find('.item-select').itemSelect());
+	this.one('mount', function() { $(this.root).find('.item-select').itemSelect(); });
 }, '{ }');
 
 riot.tag2('configuration-option', '<option class="item-select-option"> <yield></yield> </option>', '', '', function(opts) {
