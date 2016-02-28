9a605f7ebc0c07f90755d730003a1daed03fa1c3
George Hilliard <thirtythreeforty@gmail.com>
2016-02-27 19:33:54 -0600

    Slightly improve profiling tooling

--- src/common.h
+++ src/common.h
@@ -25,15 +25,18 @@ inline void debug_profile_start()
 {
 	time_ms(&start_time, &start_ms);
 }
-inline void debug_profile_end()
+inline void _debug_profile_end(const char *file, const uint16_t line)
 {
 	time_t end_time;
 	uint16_t end_ms;
 	time_ms(&end_time, &end_ms);
-	APP_DEBUG("Started @ %lu.%i, now %lu.%i (%lu ms elapsed)",
+	APP_DEBUG("On %s:%i... started @ %lu.%i, now %lu.%i (%lu ms elapsed)",
+	          file, line,
 	          start_time, start_ms, end_time, end_ms,
 	          (end_time - start_time) * 1000 + end_ms - start_ms);
 }
+#define debug_profile_end() \
+	_debug_profile_end(__FILE__, __LINE__)
 
 #else
 #define APP_DEBUG(...)
