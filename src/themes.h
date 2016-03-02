3b6b851ab22fdc8b79818eda10ed4ab966cb8ba7
George Hilliard <thirtythreeforty@gmail.com>
2016-03-02 11:34:30 -0600

    Save space on Aplite by checking for background color in theme

--- src/themes.h
+++ src/themes.h
@@ -9,25 +9,35 @@ extern "C" {
 	Type _ ## ElementName; \
 	const auto& ElementName() const { return _ ## ElementName; }
 
+#ifdef PBL_COLOR
+#define THEME_ACCESSOR_FGCOLOR(ElementName) \
+	THEME_ACCESSOR(GColor, ElementName)
+#else
+#define THEME_ACCESSOR_FGCOLOR(ElementName) \
+	GColor ElementName() const { \
+		return _background_color.argb == GColorBlack.argb ? GColorWhite : GColorBlack; \
+	}
+#endif
+
 struct Theme
 {
 	THEME_ACCESSOR(GColor, background_color);
 	THEME_ACCESSOR(uint8_t, no_bluetooth_resource);
 	THEME_ACCESSOR(GColor, tick_color);
 	THEME_ACCESSOR(uint8_t, tick_resource);
-	THEME_ACCESSOR(GColor, hour_hand_color);
-	THEME_ACCESSOR(GColor, minute_hand_color);
-	THEME_ACCESSOR(GColor, second_hand_color);
-	THEME_ACCESSOR(GColor, complication_text_color);
-	THEME_ACCESSOR(GColor, complication_icon_color);
-	THEME_ACCESSOR(GColor, complication_ring_background_color);
-	THEME_ACCESSOR(GColor, battery_complication_color);
-	THEME_ACCESSOR(GColor, health_complication_color);
-	THEME_ACCESSOR(GColor, weather_complication_humidity_color);
-	THEME_ACCESSOR(GColor, weather_complication_temp_color);
 	THEME_ACCESSOR(bool, complication_fill);
 	THEME_ACCESSOR(uint8_t, complication_ring_thickness);
 	THEME_ACCESSOR(uint8_t, minute_hand_width);
+	THEME_ACCESSOR_FGCOLOR(hour_hand_color);
+	THEME_ACCESSOR_FGCOLOR(minute_hand_color);
+	THEME_ACCESSOR_FGCOLOR(second_hand_color);
+	THEME_ACCESSOR_FGCOLOR(complication_text_color);
+	THEME_ACCESSOR_FGCOLOR(complication_icon_color);
+	THEME_ACCESSOR_FGCOLOR(complication_ring_background_color);
+	THEME_ACCESSOR_FGCOLOR(battery_complication_color);
+	THEME_ACCESSOR_FGCOLOR(health_complication_color);
+	THEME_ACCESSOR_FGCOLOR(weather_complication_humidity_color);
+	THEME_ACCESSOR_FGCOLOR(weather_complication_temp_color);
 };
 
 #undef THEME_ACCESSOR
