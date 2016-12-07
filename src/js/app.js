22fb166769b4979023eeefa6ebc07533a9dcb2f8
George Hilliard <thirtythreeforty@gmail.com>
2016-12-06 20:55:38 -0600

    Copy config to v10/

--- src/js/app.js
+++ src/js/app.js
@@ -265,7 +265,7 @@ Pebble.addEventListener('appmessage', function(e) {
 });
 
 Pebble.addEventListener('showConfiguration', function() {
-	var url = 'https://thirtythreeforty.github.io/consensus/config/v8/';
+	var url = 'https://thirtythreeforty.github.io/consensus/config/v10/';
 	console.log('Showing configuration page: ' + url);
 
 	Pebble.openURL(url);
